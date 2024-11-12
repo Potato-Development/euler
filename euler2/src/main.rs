5e32b716277720a4055e4612f72c284a78e09603
edamame-maru <so1maru.ueda@gmail.com>
2024-11-12 14:00:06 +0000

    added euler

--- euler2/src/main.rs
+++ euler2/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut sum = 0u32;
+    let mut a = 1u32;
+    let mut b = 1u32;
+    let mut c = a + b;
+
+    while c < 4000000 {
+        sum += c;
+        a = b + c;
+        b = c + a;
+        c = a + b;
+    }
+
+    println!("{}", sum);
 }
